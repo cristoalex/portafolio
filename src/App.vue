7c92d97975542e5ed6b63212d7b891613450c166
cristopher <crisramoso@gmail.com>
2025-01-12 13:43:39 -0600

    primercomit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,204 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Cris</router-link> |
+    <router-link to="/proyect">Proyectos</router-link>
   </nav>
-  <router-view/>
+  <router-view />
+
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+  integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+
+
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.derchos{
+  margin: 20px 0 0; 
+  color: #6c757d; 
+  font-size: 14px;
+}
+.bolsa{
+  width: 24px; 
+  height: 24px; 
+  border-radius: 100px;
+}
+.mani{
+  margin-bottom: 20px;
+}
+.mani a{
+  margin-right: 10px;
+}
+.camaron{
+  padding: 20px; 
+  text-align:left; 
+  border-top: 1px solid #e9ecef; 
+  color: antiquewhite; 
+  background-color: transparent;
 }
 
 nav {
   padding: 30px;
+  
 }
 
 nav a {
   font-weight: bold;
+  font-size: large;
   color: #2c3e50;
 }
 
 nav a.router-link-exact-active {
   color: #42b983;
 }
+
+#app {
+  text-align: left;
+  padding-left: 30px;
+  padding-right: 30px;
+  padding-top: 60px;
+}
+body {
+  background-image: url('/src/assets/fondo.jpeg');
+  background: linear-gradient(to left, transparent, #09063a),
+    url("/src/assets/fondo.jpeg");
+  background-size: cover;
+  /* Escala la imagen para cubrir el área */
+  background-position: center;
+  /* Centra la imagen */
+  background-repeat: no-repeat;
+  
+}
+
+.arbol {
+
+  flex-wrap: wrap;
+  align-items: center;
+}
+.mango {
+
+  width: 200px;
+  padding: auto; 
+  float: right;
+}
+
+.gato{
+  padding: 5em;
+}
+
+.panda {
+  padding: 10px;
+  text-align: center;
+  transition: transform 0.2s ease-in-out;
+  color: antiquewhite;
+}
+.panda img {
+  border-radius: 10px; 
+  max-width: 100px; 
+  height: auto; 
+  margin-bottom: 1em;
+}
+
+
+main {
+  display: grid;
+  grid-gap: 1em;
+  padding: 1em;
+}
+/* Estilos para las cajas individuales */
+.box {
+  background-color: #eee;
+  border-radius: 5px;
+  padding: 1em;
+}
+
+.box img {
+  max-width: 100%;
+  height: auto;
+}
+
+/* Estilos para las cajas individuales */
+.box {
+  background-color: #09063a;
+  border-radius: 5px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+  padding: 1em;
+  text-align: center;
+  transition: transform 0.2s ease-in-out;
+}
+
+.box img {
+  max-width: 100%;
+  height: auto;
+  border-radius: 5px;
+  margin-bottom: 1em;
+}
+
+.box p {
+  font-size: 1em;
+  font-style: italic;
+  color: antiquewhite;
+}
+
+.box:hover {
+  transform: translateY(-5px);
+}
+
+/* Estilos para pantallas más pequeñas */
+@media (max-width: 1700px) {
+
+  /* Cambiar a dos columnas */
+  main {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(3, auto);
+    gap: 1em;
+  }
+
+  .gato{
+
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-template-rows: repeat(2, auto);
+    gap: 1em;
+
+  }
+
+
+}
+
+/* Estilos para pantallas más pequeñas */
+@media (max-width: 480px) {
+
+  /* Cambiar a una sola columna */
+  main {
+    display: flex;
+    flex-direction: column;
+  }
+  .gato{
+    display: flex;
+    flex-direction: column;
+  }
+    .mango {
+      float: none;
+      display: block;
+      margin: 0 auto 1em;
+      max-width: 80%;
+    }
+
+  /* Ajustar tamaño de las cajas */
+  .box {
+    margin-bottom: 1em;
+  }
+}
+
+.zoomable-image {
+  transition: transform 0.5s ease;
+}
+
+.zoomable-image:hover {
+  transform: scale(1.2);
+}
+
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,72 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<div class="arbol">
+  <img class="mango" src="../assets/no-image.png" width="200">
+  <p style="color:beige; text-align: center;"></p>
+
+</div>
+  <p style="color:beige; text-align: center;">
+
+  <ul style="color:antiquewhite; text-align: left; font-family: monospace;">
+
+    <ul style="color: whitesmoke;">
+      <h2>Actitudes y Conocimientos</h2>
+    </ul>
+    <li>
+      <h6>Diseño Web: CSS, Bootstrap.</h6>
+    </li>
+    <li>
+      <h6>Lenguajes de Programación: PHP, SQL, Python (básico), C#, JavaScript.</h6>
+    </li>
+    <li>
+      <h6>Bases de Datos: Diseño, administración y optimización de bases de datos.</h6>
+    </li>
+    <li>
+      <h6>Gestión de Proyectos: Conocimiento en metodologías de desarrollo ágil y técnicas básicas de gestión.</h6>
+    </li>
+    <li>
+      <h6>Área de Sistemas: Experiencia general en tecnologías y soluciones relacionadas con sistemas informáticos.</h6>
+    </li>
+
+  </ul>
+  </p>
+
+
+
+
+  <div class="gallery">
+    <div class="row">
+
+      <main class="gato">
+        <div class="panda">
+          <img src="../assets/logo.png" alt="">
+          <p><h3></h3></p>
+        </div>
+        <div class="panda">
+          <img src="../assets/html.png" alt="">
+          <p><h3></h3></p>
+        </div>
+        <div class="panda">
+          <img src="../assets/css.png" alt="">
+          <p><h3></h3></p>
+        </div>
+        <div class="panda">
+          <img src="../assets/java.jpg" alt="">
+          <p><h3></h3></p>
+        </div>
+        <div class="panda">
+          <img src="../assets/sql.png" alt="">
+          <p><h3></h3></p>
+        </div>
+      </main>
+    </div>
   </div>
+
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
 </script>
+
+<style>
+</style>
